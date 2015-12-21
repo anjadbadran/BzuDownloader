1557c2306ed41367b946205534bee3e4ab6abc58
mohammad tomaizy <hamados@ritaj.ps>
2015-12-21 20:16:36 +0200

    add real segments  links

---
+++ src/com/bzu/softwareconstruction/logic/MainLogic.java
@@ -0,0 +1,18 @@
+package com.bzu.softwareconstruction.logic;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.bzu.softwareconstruction.segments.SegmentPartUrl;
+import com.bzu.softwareconstruction.services.Services;
+
+
+public class MainLogic {
+	public static void main(String args[]) throws FileNotFoundException{
+		//Segment segment = "/test_data/text_test.segments")));
+		String baseUrl = "http://151.80.134.50/data/";
+		Services.multipartDownloader.download(new SegmentPartUrl(baseUrl+"text_test.segments"), new File("./test_output/"));
+	//	Services.multipartDownloader.download(new SegmentPartUrl(baseUrl+"test_data2/test2.segments"), new File("./test_output/"));
+
+	}
+}
